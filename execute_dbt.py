e729dcbc30fcf8eecf3df362fe057261213d4367
Blake Burch <blake@shipyardapp.com>
2020-12-14 15:40:07 -0600

    Update execution to assume current directory for commands.

--- execute_dbt.py
+++ execute_dbt.py
@@ -1,6 +1,8 @@
 import subprocess
 import os
 
-dbt_command = os.environ.get('DBT_COMMAND')
-subprocess.run(["pwd"])
+dbt_command = os.environ.get('DBT_COMMAND', 'dbt run')
+
+directory_of_file = os.path.dirname(os.path.realpath(__file__))
+os.chdir(directory_of_file)
 subprocess.run(dbt_command.split(' '))
