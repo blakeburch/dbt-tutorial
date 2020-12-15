55886533c0584ac71ce45e1c8e81576d2115d72f
Blake Burch <blake@shipyardapp.com>
2020-12-15 12:19:09 -0600

    Create BQ creds file

--- execute_dbt.py
+++ execute_dbt.py
@@ -1,8 +1,14 @@
 import subprocess
 import os
+import json
 
 dbt_command = os.environ.get('DBT_COMMAND', 'dbt run')
 
 directory_of_file = os.path.dirname(os.path.realpath(__file__))
 os.chdir(directory_of_file)
-subprocess.run(dbt_command.split(' '))
+
+bigquery_credentials = os.environ.get('BIGQUERY_CREDS')
+with open('bigquery_creds.json', 'w') as outfile:
+    json.dump(bigquery_credentials, outfile)
+
+subprocess.run(['sh', '-c', dbt_command])
