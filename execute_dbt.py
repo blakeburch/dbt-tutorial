7f3f8314e930abf77ecf4dce9a9d683d3ebb0bdf
Blake Burch <blake@shipyardapp.com>
2020-12-15 12:24:10 -0600

    Create JSON in cwd before switch

--- execute_dbt.py
+++ execute_dbt.py
@@ -4,11 +4,12 @@ import json
 
 dbt_command = os.environ.get('DBT_COMMAND', 'dbt run')
 
-directory_of_file = os.path.dirname(os.path.realpath(__file__))
-os.chdir(directory_of_file)
-
 bigquery_credentials = os.environ.get('BIGQUERY_CREDS')
 with open('bigquery_creds.json', 'w') as outfile:
     json.dump(bigquery_credentials, outfile)
+
+directory_of_file = os.path.dirname(os.path.realpath(__file__))
+os.chdir(directory_of_file)
+
 
 subprocess.run(['sh', '-c', dbt_command])
