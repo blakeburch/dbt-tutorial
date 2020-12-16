2e1d7ab0dabd0d42eabd6e3a7bd20d3d784118d7
Blake Burch <blake@shipyardapp.com>
2020-12-16 12:31:38 -0600

    Change bigquery credentials location.

--- execute_dbt.py
+++ execute_dbt.py
@@ -8,11 +8,11 @@ dbt_command = os.environ.get(
 
 bigquery_credentials = os.environ.get('BIGQUERY_CREDS')
 bigquery_credentials = json.loads(bigquery_credentials)
-with open('bigquery_creds.json', 'w') as outfile:
-    json.dump(bigquery_credentials, outfile)
 
 directory_of_file = os.path.dirname(os.path.realpath(__file__))
 os.chdir(directory_of_file)
+with open('bigquery_creds.json', 'w') as outfile:
+    json.dump(bigquery_credentials, outfile)
 
 
 subprocess.run(['sh', '-c', dbt_command])
