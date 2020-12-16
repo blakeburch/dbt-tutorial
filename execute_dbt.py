8021eecbf0b1ec02e612c8ab5b9ee45798bab003
Blake Burch <blake@shipyardapp.com>
2020-12-16 15:18:02 -0600

    Change creation location of BQ creds

--- execute_dbt.py
+++ execute_dbt.py
@@ -25,10 +25,10 @@ else:
 
     bigquery_credentials = os.environ.get('BIGQUERY_CREDS')
     bigquery_credentials = json.loads(bigquery_credentials)
-    with open('bigquery_creds.json', 'w') as outfile:
-        json.dump(bigquery_credentials, outfile)
 
     directory_of_file = os.path.dirname(os.path.realpath(__file__))
     os.chdir(directory_of_file)
+    with open('bigquery_creds.json', 'w') as outfile:
+        json.dump(bigquery_credentials, outfile)
 
     subprocess.run(['sh', '-c', dbt_command])
